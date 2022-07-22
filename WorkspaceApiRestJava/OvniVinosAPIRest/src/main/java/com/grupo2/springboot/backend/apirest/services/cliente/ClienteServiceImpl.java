ccaf66a9140f6fd02126347fbeb22d2d0f455639
= <>
2022-07-22 15:39:43 -0500

    Crud de producto

--- WorkspaceApiRestJava/OvniVinosAPIRest/src/main/java/com/grupo2/springboot/backend/apirest/services/cliente/ClienteServiceImpl.java
+++ WorkspaceApiRestJava/OvniVinosAPIRest/src/main/java/com/grupo2/springboot/backend/apirest/services/cliente/ClienteServiceImpl.java
@@ -1,5 +1,24 @@
 package com.grupo2.springboot.backend.apirest.services.cliente;
 
-public class ClienteServiceImpl {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.grupo2.springboot.backend.apirest.dao.IAdministradorDao;
+import com.grupo2.springboot.backend.apirest.dao.IClienteDao;
+import com.grupo2.springboot.backend.apirest.entity.AdministradorVo;
+import com.grupo2.springboot.backend.apirest.entity.ClienteVo;
+
+public class ClienteServiceImpl implements IClienteService{
+
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<ClienteVo> findByCorreo(String correo) {
+		
+		return clienteDao.findById(correo);
+	}
 }
